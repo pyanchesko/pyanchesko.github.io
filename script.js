1e8536f4c017557c147710f36084f070bcea26ef
Nikita Piankov <nikita.piankov@semrush.com>
2023-04-01 15:43:04 +0200

    new generation

--- script.js
+++ script.js
@@ -1,56 +1,38 @@
-const swipeContainer = document.querySelector('.swipe-container');
-const swipeContent = document.querySelector('.swipe-content');
-const swipeItems = document.querySelectorAll('.swipe-item');
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("container");
+  const swipeContents = [
+      { content: "Swipeable Content 1", backgroundColor: "red" },
+      { content: "Swipeable Content 2", backgroundColor: "blue" },
+      { content: "Swipeable Content 3", backgroundColor: "green" },
+  ];
 
-let currentSlide = 0;
-let startX;
-let startY;
-let distX;
-let distY;
-let threshold = 50; // minimum distance to trigger swipe
-let itemWidth = swipeContainer.clientWidth / swipeItems.length;
+  let currentIndex = 0;
 
-swipeContent.style.width = `${swipeItems.length * 100}%`;
-
-for (let i = 0; i < swipeItems.length; i++) {
-  swipeItems[i].style.width = `${itemWidth}px`;
-}
-
-swipeContainer.addEventListener('touchstart', handleTouchStart);
-swipeContainer.addEventListener('touchmove', handleTouchMove);
-swipeContainer.addEventListener('touchend', handleTouchEnd);
-
-function handleTouchStart(event) {
-  console.log("touchstart");
-  startX = event.touches[0].clientX;
-  startY = event.touches[0].clientY;
-}
-
-function handleTouchMove(event) {
-  console.log("touchmove");
-  distX = event.touches[0].clientX - startX;
-  distY = event.touches[0].clientY - startY;
-
-  if (Math.abs(distX) > Math.abs(distY)) {
-    event.preventDefault();
-    swipeContent.style.transform = `translateX(-${currentSlide * itemWidth}px) translateX(${distX}px)`;
-  }
-}
-
-function handleTouchEnd(event) {
-  console.log("touchend");
-  if (Math.abs(distX) > threshold) {
-    if (distX > 0 && currentSlide > 0) {
-      currentSlide--;
-    } else if (distX < 0 && currentSlide < swipeItems.length - 1) {
-      currentSlide++;
-    }
-  console.log(currentSlide);
+  function createSwipeContent(content, backgroundColor) {
+      const swipeContent = document.createElement("div");
+      swipeContent.classList.add("swipe-content");
+      swipeContent.textContent = content;
+      swipeContent.style.backgroundColor = backgroundColor;
+      return swipeContent;
   }
 
-  swipeContent.style.transform = `translateX(-${currentSlide * itemWidth}px)`;
-  startX = null;
-  startY = null;
-  distX = null;
-  distY = null;
-}
+  function updateSwipeContent() {
+      container.innerHTML = "";
+      container.appendChild(createSwipeContent(swipeContents[currentIndex].content, swipeContents[currentIndex].backgroundColor));
+  }
+
+  function navigateContent(direction) {
+      if (direction === "left") {
+          currentIndex = (currentIndex - 1 + swipeContents.length) % swipeContents.length;
+      } else if (direction === "right") {
+          currentIndex = (currentIndex + 1) % swipeContents.length;
+      }
+      updateSwipeContent();
+  }
+
+  updateSwipeContent();
+
+  const hammer = new Hammer(container);
+  hammer.on("swipeleft", () => navigateContent("left"));
+  hammer.on("swiperight", () => navigateContent("right"));
+});
